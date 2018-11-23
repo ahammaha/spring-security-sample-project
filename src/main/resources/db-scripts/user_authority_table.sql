b9ff360d768e53e8f3eb918c8801fad4e5445302
ahammaha <mahalakshmi@outlook.com>
2018-11-23 11:38:55 +0530

    encrypting pwd using bcrypt

--- src/main/resources/db-scripts/user_authority_table.sql
+++ src/main/resources/db-scripts/user_authority_table.sql
@@ -1,16 +1,16 @@
-DROP DATABASE  IF EXISTS `sample_spring_security`;
+DROP DATABASE  IF EXISTS `spring_security_bcrypt_ex`;
 
-CREATE DATABASE  IF NOT EXISTS `sample_spring_security`;
-USE `sample_spring_security`;
+CREATE DATABASE  IF NOT EXISTS `spring_security_bcrypt_ex`;
+USE `spring_security_bcrypt_ex`;
 
 --
 -- Table structure for table `users`
---
+-- Password field should have min 68 chars because {bcrypt} => 8 char + 60 char for encrypted pwd
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(68) NOT NULL,
   `enabled` tinyint(1) NOT NULL,
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -21,9 +21,9 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` 
 VALUES 
-('user1','{noop}pwd',1),
-('user2','{noop}pwd',1),
-('user3','{noop}pwd',1);
+('user1','{bcrypt}$2a$04$Oud190SfbiHMfBv6dt4CPem2TqgVaq1wvyC5rrupenm9zbB8SstQu',1),
+('user2','{bcrypt}$2a$04$Oud190SfbiHMfBv6dt4CPem2TqgVaq1wvyC5rrupenm9zbB8SstQu',1),
+('user3','{bcrypt}$2a$04$Oud190SfbiHMfBv6dt4CPem2TqgVaq1wvyC5rrupenm9zbB8SstQu',1);
 
 
 --
@@ -49,4 +49,3 @@ VALUES
 ('user2','ROLE_MANAGER'),
 ('user2','ROLE_EMPLOYEE'),
 ('user3','ROLE_EMPLOYEE');
-
